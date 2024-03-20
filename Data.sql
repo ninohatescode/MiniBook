ae483ff7ec602cef622941cdfc379e02c828e62a
LunarQuinnie <128813315+LunarQuinnie@users.noreply.github.com>
2024-03-20 15:42:46 +0700

    Update Data.sql

--- Data.sql
+++ Data.sql
@@ -1,28 +1,28 @@
-﻿insert into HRM(hrm_user_name,hrm_password)
+﻿insert into QUANLI(TenTaiKhoan,MatKhau)
 values
 ('admin','admin')
 
-SET IDENTITY_INSERT Category ON;
-insert into Category(category_id, category_name)
+SET IDENTITY_INSERT THELOAI ON;
+insert into THELOAI(IDTheLoai, TenTheLoai)
 values
 (1, N'Kỹ năng sống'),
 (2, N'Rèn luyện nhân cách'),
 (3, N'Tâm lý'),
 (4, N'Sách cho tuổi mới lớn')
-SET IDENTITY_INSERT Category OFF;
+SET IDENTITY_INSERT THELOAI OFF;
 
-SET IDENTITY_INSERT Publisher ON;
-insert into Publisher(publisher_id, publisher_phone, publisher_mail, publisher_address, publisher_name)
+SET IDENTITY_INSERT NXB ON;
+insert into NXB(IDNXB, SDT, Mail, DiaChi, TenNXB)
 values
 (1, '02437734544', 'chinhanhhanoi@nxbtre.com.vn', N'Số 21, Dãy A11, Khu Đầm Trấu, Phường Bạch Đằng, Quận Hai Bà Trưng, Hà Nội ', N'Nhà xuất bản Trẻ'),
 (2, '02439428653', 'info@nxbkimdong.com.vn ', N'55 Quang Trung, Hà Nội, Việt Nam ', N'Nhà xuất bản Kim Đồng'),
 (3, '02838256804', 'nstonhop@gmail.com', N'62 Nguyễn Thị Minh Khai, Phường Đa Kao, Quận 1, TP. HCM', N'Nhà xuất bản Tổng hợp thành phố Hồ Chí Minh'),
 (4, '02438222135', 'nhaxuatbanhnv@gmail.com', N'số 65 Nguyễn Du, Quận Hai Bà Trưng, Hà Nội', N'Nhà xuất bản Hội Nhà văn'),
 (5, '02438221581', 'phathanh@nxbctqg.vn', N'6/86 Duy tân, Cầu Giấy, Hà Nội', N'Nhà xuất bản Chính trị quốc gia Sự thật')
-SET IDENTITY_INSERT Publisher OFF;
+SET IDENTITY_INSERT NXB OFF;
 
-SET IDENTITY_INSERT Author ON;
-insert into Author(author_id, author_name, author_address, author_mail, author_descrip)
+SET IDENTITY_INSERT TACGIA ON;
+insert into TACGIA(IDTG, Ten, DiaChi, Mail, GioiThieu)
 values
 (1, N'Lê Đỗ Quỳnh Hương', N'Long An', 'ledoquynhhuong@gmail.com', N'MC, Thạc sĩ âm nhạc, Tác giả sách và là một diễn giả.'),
 (2, N'Minh Niệm', N'Thành phố Hồ Chí Minh', 'minhniem@gmail.com', N'Tác giả viết sách'),
@@ -40,9 +40,9 @@ values
 (14, N'Matt Crossick', N'Thành phố Hồ Chí Minh', 'minhniem@gmail.com', N'Tác giả viết sách'),
 (15, N'Lê Quang', N'Thành phố Hồ Chí Minh', 'minhniem@gmail.com', N'Tác giả viết sách'),
 (16, N'Đào Nhiên Nhiên', N'Thành phố Hồ Chí Minh', 'minhniem@gmail.com', N'Tác giả viết sách')
-SET IDENTITY_INSERT Author OFF;
+SET IDENTITY_INSERT TACGIA OFF;
 
-insert into Role(author_id, book_id, _role)
+insert into VAITRO(IDTG, IDSach, VaiTro)
 values
 (1,1, N'Tác giả'),
 (2,2, N'Tác giả'),
@@ -61,8 +61,8 @@ values
 (15,15, N'Tác giả'),
 (16,16, N'Tác giả')
 
-SET IDENTITY_INSERT Book ON;
-insert into Book(book_id, book_name, book_price, book_quantity, book_descrip, book_image, book_release, book_view, book_sale, publisher_id, category_id)
+SET IDENTITY_INSERT SACH ON;
+insert into SACH(IDSach, TenSach, GiaBan, SoLuongKho, MoTa, AnhMinhHoa, NgayPhatHanh, SLXem, SLBan, IDNXB, IDTheLoai)
 values
 (1, N'Thay Đổi Cuộc Sống Với Nhân Số Học', 148000, 100, N'Sách về thần số học', 'thaydoicuocsongvoinhansohoc.jpg','2023', 100, 12, 2, 1),
 (2, N'Hiểu Về Trái Tim',140000,100, N'Sách về nhận thức','hieuvetraitim.jpg', '2023', 100, 12, 2, 1),
@@ -80,4 +80,4 @@ values
 (14, N'Giải Ngố Cho Con Trai',57000,100, N'Sách về giải ngố', 'giaingochocontrai.jpg', '2023',100,12,2,4),
 (15, N'Kỹ Năng Sống - Cách Để Trở Thành - Cách Để Tự Bảo Vệ Mình',52000,100, N'Sách về kỹ năng sống', 'kynangsongcachdetrothanhcachdetubaoveminh.jpg', '2023',100,12,2,4),
 (16, N'Nhật Ký Trưởng Thành Của Đứa Con Ngoan - Con Học Cách Ứng Xử',42000,100, N'Sách về học cách ứng xử', 'nhatkytruongthanhcuaduaconngoan.jpg', '2023',100,12,2,4)
-SET IDENTITY_INSERT Book OFF;
+SET IDENTITY_INSERT SACH OFF;
